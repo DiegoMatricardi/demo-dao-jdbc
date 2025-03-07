bb2565de24aea69909b43a11db83c53260bcc396
Diego Matricardi <diegobmatricardi@hotmail.com>
2025-03-06 22:08:06 -0300

    Seller entity class

--- demo-dao-jdbc/src/Main.java
+++ demo-dao-jdbc/src/Main.java
@@ -1,8 +1,13 @@
 import Entities.Departament;
+import Entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Departament departament = new Departament(1, "Books");
+        Seller seller = new Seller(21,"Bob", "bog@gmail.com", new Date(), 3000.0, departament);
+        System.out.println(seller);
         System.out.println(departament);
     }
 }
